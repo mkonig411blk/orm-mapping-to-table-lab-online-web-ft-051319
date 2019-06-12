ee53652550dc86dee5a10c11f419672f2352efa0
Margot Konig <margot.konig@blackrock.com>
2019-06-12 22:42:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,10 @@
 class Student
   attr_accessor :name, :grade
   
-  def initialize
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
   end
 
   # Remember, you can access your database connection anywhere in this class
