3248790a2fff83ad964b7d784c3818bc3e21ced7
Margot Konig <margot.konig@blackrock.com>
2019-06-12 22:43:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,14 @@ class Student
   end
   
   def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
 
   # Remember, you can access your database connection anywhere in this class
